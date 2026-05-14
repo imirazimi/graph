3c3d223964a2cc4b1089aa11f64fa3e534f1d71d
Iman MirAzimi <imirazimi@gmail.com>
2026-05-15 02:24:02 +0330

    feat: add task domain model and request validation schemas

---
+++ internal/task/entity/task.go
@@ -0,0 +1,23 @@
+package entity
+
+import (
+    "time"
+
+    "github.com/google/uuid"
+)
+
+const (
+    StatusTodo  = "todo"
+    StatusDoing = "doing"
+    StatusDone  = "done"
+)
+
+type Task struct {
+    ID          uuid.UUID `json:"id"`
+    Title       string    `json:"title"`
+    Description string    `json:"description"`
+    Status      string    `json:"status"`
+    Assignee    string    `json:"assignee"`
+    CreatedAt   time.Time `json:"created_at"`
+    UpdatedAt   time.Time `json:"updated_at"`
+}
